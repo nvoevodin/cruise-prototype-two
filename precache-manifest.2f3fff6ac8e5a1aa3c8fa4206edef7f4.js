2750b16d684cfd706312de32922bda5b94a1e506
nvoevodin <46409820+nvoevodin@users.noreply.github.com>
2020-12-22 16:36:19 -0500

    Add files via upload

---
+++ precache-manifest.2f3fff6ac8e5a1aa3c8fa4206edef7f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454404bc7ab1a52794cb6d379a10db07",
+    "url": "/cruise-prototype-two/index.html"
+  },
+  {
+    "revision": "f2a5e231e999de698f78",
+    "url": "/cruise-prototype-two/static/css/2.daed29bd.chunk.css"
+  },
+  {
+    "revision": "9625ce4209af491a973e",
+    "url": "/cruise-prototype-two/static/css/main.2b0b8d74.chunk.css"
+  },
+  {
+    "revision": "f2a5e231e999de698f78",
+    "url": "/cruise-prototype-two/static/js/2.461a837a.chunk.js"
+  },
+  {
+    "revision": "372c41146dc8c33cc99f",
+    "url": "/cruise-prototype-two/static/js/3.5fceee43.chunk.js"
+  },
+  {
+    "revision": "9625ce4209af491a973e",
+    "url": "/cruise-prototype-two/static/js/main.18eab0bc.chunk.js"
+  },
+  {
+    "revision": "4f543a72d554427199cc",
+    "url": "/cruise-prototype-two/static/js/runtime-main.056afe28.js"
+  }
+]);
